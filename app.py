df175479b0054f9de5b0c199492beab47fb8c2b5
Kaloyan Anastasov <kaloanas07@gmail.com>
2023-05-08 20:07:00 +0300

    cleaned up the code

--- app.py
+++ app.py
@@ -26,51 +26,35 @@ def read_image(content: bytes) -> np.ndarray:
     return image
 
 st.title('Cancer Segmentation')
-st.info('This is a simple example of using a trained ML model to make predictions on images.')
+st.info('This is a simple example of using a trained ML model to make predictions for cancer segmentation on images.')
 
-width = st.sidebar.slider("plot width", 1, 40, 3)
-height = st.sidebar.slider("plot height", 1, 40, 1)
+# width = st.sidebar.slider("plot width", min_value=10, max_value=50, step=5, value=30)
+# height = st.sidebar.slider("plot height", min_value=10, max_value=50, step=5, value=20)
 
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
     # To read file as bytes:
     bytes_data = uploaded_file.getvalue()
-    # print(bytes_data)
     
     url = "http://127.0.0.1:8000/"
 
     payload = {}
-    # files=[
-    # ('data',('testimg.jpg',open('C:/Users/sas/Desktop/CancerDetection/testimg.jpg','rb'),'image/jpeg'))
-    # ]
     files=[('data', bytes_data)]
     headers = {
     'accept': 'application/json'
     }
 
     response = requests.request("POST", url, headers=headers, data=payload, files=files)
-
-    print(response.text)
     
     prediction = json.loads(response.text)
     
     yhat = np.array(json.loads(prediction['prediction']))
     yhat = np.squeeze(np.where(yhat > 0.3, 1.0, 0.0))
     
-    print(f'yhat -> {yhat}')
-    
-    # img = cv.imdecode(yhat, flags=1)
-    
-    # x = cv2.imread('testimg.jpg')
-    
     result_image = read_image(bytes_data)
-    
-    
-    fig, ax = plt.subplots(1,7, figsize=(width, height))
+      
+    fig, ax = plt.subplots(1, 7, figsize=(20, 10))
     ax[0].imshow(result_image) 
     for i in range(6):
         ax[i+1].imshow(yhat[:,:,i])
     st.pyplot(fig)
-    
-    print('Done')
-    
